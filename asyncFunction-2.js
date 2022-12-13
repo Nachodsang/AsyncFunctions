90d4a7527835cd112b368596832faca82a7dac65
Nachodsang <nsanguansataya@gmail.com>
2022-12-13 22:41:34 +0700

    trial deleted

--- asyncFunction-2.js
+++ asyncFunction-2.js
@@ -15,27 +15,3 @@ const asyncFunction2 = (argFunction) => {
   }
 };
 asyncFunction2(hello);
-
-// const asyncFunction2 = (argFunction) => {
-//   const valid = typeof argFunction === "function";
-//   new Promise((resolve, reject) => {
-//     if (valid) {
-//       resolve(() => {
-//         setTimeout(() => {
-//           argFunction();
-//           console.log("callback resolved after 3 seconds");
-//         }, 3000);
-//       });
-//     }
-//     reject(() => {
-//       console.log("not a function");
-//     });
-//   });
-// };
-// asyncFunction2(hello)
-//   .then((respond) => {
-//     respond();
-//   })
-//   .catch((error) => {
-//     console.log(error);
-//   });
